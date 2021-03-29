1dd949a0e96306ffa132a291a9a02b0e07e6c030
ghazaputra <70355895+ghazaputra@users.noreply.github.com>
2021-03-29 11:22:09 +0700

    Add files via upload

---
+++ Youtube Clone/Helper/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Youtube Clone
+//
+//  Created by ADMIN on 17/03/21.
+//
+
+import Foundation
+
+struct Constants {
+  static var API_KEY = "AIzaSyAVpl2CugmYOTgybzFFeh2zIPnJZbZJ6fo"
+  static var PLAYLIST_ID = "UU0VsGy54n1tJDUtU6sssO0w"
+  static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+  static var VIDEOCEL_ID = "VideoCell"
+
+}
+//ini untuk menyimpan link
